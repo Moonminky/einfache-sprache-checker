c6643a95b886c043ca69d2ec661101888696b005
Moonminky <noremori@gmail.com>
2022-11-25 10:56:42 +0100

    Footer added

--- sprachchecker/src/App.js
+++ sprachchecker/src/App.js
@@ -1,17 +1,19 @@
 import "./App.css";
 import TextInput from "./components/TextInput";
 import AnalysisResult from "./components/AnalysisResult";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="header">
         <h1>Einfache Sprache Quick-Check</h1>
       </header>
       <section className="textanalysis">
         <TextInput className="textinput" />
         <AnalysisResult className="analysisresult" />
       </section>
+      <Footer />
     </div>
   );
 }
---
+++ sprachchecker/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>
+        Developed with &#9749; by{" "}
+        <a
+          href="https://github.com/Moonminky"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Eleonore Brudaru{" "}
+        </a>
+      </p>
+      <p>Copyright &copy; 2022</p>
+    </footer>
+  );
+};
+
+export default Footer;
