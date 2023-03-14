492a0da65c663f9db614916966fb411fb3dbfedf
Moonminky <noremori@gmail.com>
2023-03-14 11:15:25 +0100

    finished unit tests for TextInput component

--- sprachchecker/src/TextInput.test.js
+++ sprachchecker/src/TextInput.test.js
@@ -1,38 +1,54 @@
-import { render, screen, fireEvent, waitFor, userEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import React from "react";
 import TextInput from "./components/TextInput";
-import { toast } from "react-toastify";
+import App from "./App";
 
 describe("TextInput", () => {
-  it("renders the component correctly", () => {
-    const { getByLabelText } = render(<TextInput />);
-    expect(getByLabelText("Wähle das Sprachlevel:")).toBeInTheDocument();
+  it("calls onSend with expected arguments when form is submitted", async () => {
+    const onSendMock = jest.fn();
+    const { getByTestId, getByLabelText } = render(
+      <TextInput onSend={onSendMock} />
+    );
+    const contentEditable = getByTestId("content-input");
+    const selectElement = getByLabelText("Wähle das Sprachlevel:");
+    const form = contentEditable.closest("form");
+
+    fireEvent.change(selectElement, { target: { value: "a2" } });
+    const text = "Some text";
+    contentEditable.innerHTML = text;
+    const range = document.createRange();
+    range.selectNodeContents(contentEditable);
+    range.collapse(false);
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    fireEvent.input(contentEditable, { target: { innerHTML: text } });
+
+    await waitFor(() => expect(contentEditable.innerHTML).toEqual(text));
+
+    fireEvent.submit(form);
+
+    await waitFor(() =>
+      expect(onSendMock).toHaveBeenCalledWith({
+        level: "a2",
+        normalizedText: "Some text"
+      })
+    );
   });
 
-  //TODO:implement
-  // it("calls onSend when submitted with text input", async () => {
-  //   // declare as async function
-  //   const onSend = jest.fn();
-  //   const user = userEvent.setup();
-  //   const { getByText, getByTestId } = render(<TextInput onSend={onSend} />);
-  //   const input = getByTestId("content-input");
-  //   const button = getByText("Check");
-  //   await user.type(input, "Hello world"); // use userEvent.type
-  //   await user.click(button);
-  //   expect(input.value).toBe("Hello world"); // use value instead of textContent
-  //   expect(onSend).toHaveBeenCalledWith({
-  //     text: "Hello world",
-  //     level: "a1"
-  //   });
-  // });
+  it("shows an error message when submitted with no text input", async () => {
+    // Render the App component
+    render(<App />);
 
-  it("shows an error message when submitted with no text input", () => {
-    //TODO: test if toast.error is shown
-    // const onSend = jest.fn();
-    // const { getByText } = render(<TextInput onSend={onSend} />);
-    // const button = getByText("Check");
-    // fireEvent.click(button);
-    // expect(toast.error).toHaveBeenCalled();
+    // Simulate a form submission without input
+    const submitButton = screen.getByRole("button", { name: /check/i });
+    fireEvent.click(submitButton);
+
+    // Check if the toast error message element is present in the DOM
+    await waitFor(() => {
+      const toast = screen.getByRole("alert");
+      expect(toast).toHaveTextContent("Bitte gib zuerst Text ein!");
+    });
   });
 
   it("renders highlighted text with given submittedText and highlights", async () => {
